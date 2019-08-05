234726b2263fd386b0093e234440e4fb94990b2a
ameliaknipp <ameliaknipp@aol.com>
2019-08-05 23:49:28 +0000

    Devise Gem Installed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'https://grammable-amelia-knipp.herokuapp.com/
+' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
