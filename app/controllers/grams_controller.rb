79dcac74955f8335add03fef394014c4bd4032ef
ameliaknipp <ameliaknipp@aol.com>
2019-08-06 23:29:45 +0000

    Carrierwave

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -13,7 +13,7 @@ class GramsController < ApplicationController
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
     return render_not_found(:forbidden) if @gram.user != current_user
-    
+
     @gram.update_attributes(gram_params)
 
   if @gram.valid?
@@ -24,6 +24,7 @@ class GramsController < ApplicationController
   end
 
   def index
+    @grams = Gram.all
   end
 
   def new
@@ -53,10 +54,10 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
+    render plain: "#{status.to_s.titleize} (", status: status
   end
 end
